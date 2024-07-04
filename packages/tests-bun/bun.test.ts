115a149f7321321ec6eda37a3027c87e1264582d
Sergei Blinov <blinovsv@gmail.com>
2024-07-04 08:01:50 +0200

    dummy bun test wasm

--- packages/tests-bun/bun.test.ts
+++ packages/tests-bun/bun.test.ts
@@ -7,22 +7,27 @@ test("init client with lib-web and separate worker disabled", async () => {
 	// Disable separate worker
 	libWebSetup({
 		disableSeparateWorker: true,
-		binaryURL: Bun.pathToFileURL("../lib-web/eversdk.wasm"),
+		binaryURL: Bun.pathToFileURL("node_modules/@eversdk/lib-web/eversdk.wasm").toString(),
 	});
-	//
 
 	TonClient.useBinaryLibrary(libWeb);
 
 	const client = new TonClient();
 	const keys = await client.crypto.generate_random_sign_keys();
-	expect(keys.public.length > 12).toBeTrue();
+	expect(keys.public.length > 20).toBeTrue();
+
+    const count = 12;
+    const { phrase } = await client.crypto.mnemonic_from_random({
+        word_count: count,
+        dictionary: 1,
+    });
+    expect(phrase.split(" ").length).toBe(count);
 });
 
-test("init client with lib-web and separate worker enabled", async () => {
+test.todo("init client with lib-web and separate worker enabled", async () => {
 	libWebSetup({
-		binaryURL: Bun.pathToFileURL("../lib-web/eversdk.wasm"),
+		binaryURL: Bun.pathToFileURL("../lib-web/eversdk.wasm").toString(),
 	});
-	//
 
 	TonClient.useBinaryLibrary(libWeb);
 
